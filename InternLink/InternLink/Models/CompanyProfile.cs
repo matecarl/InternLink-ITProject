d6914304db7475e640d5b2acf00e6f9ffd23846d
Karim Altir <mhd.altir@student.ehb.be>
2024-12-22 23:28:09 +0100

    making the models for the student and company profile

--- InternLink/InternLink/Models/CompanyProfile.cs
+++ InternLink/InternLink/Models/CompanyProfile.cs
@@ -1,7 +1,6 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using InternLink.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Build.Framework;
 
 namespace InternLink.Models;
 
@@ -12,9 +11,7 @@ public class CompanyProfile
     [ForeignKey(nameof(User))]
     public string UserId { get; set; }
     public ApplicationUser? User { get; set; }
-    [Required]
     public string CompanyName { get; set; }
-    [Required]
     public string Industry { get; set; }
     public string WebsiteUrl { get; set; }
 
--- InternLink/InternLink/Models/StudentProfile.cs
+++ InternLink/InternLink/Models/StudentProfile.cs
@@ -1,7 +1,6 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using InternLink.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Build.Framework;
 
 namespace InternLink.Models;
 
@@ -11,15 +10,12 @@ public class StudentProfile
     [Required]
     [ForeignKey(nameof(User))]
     public string UserId { get; set; }
+    public string UserName { get; set; }
     public ApplicationUser? User { get; set; }
-    [Required]
     public string FirstName { get; set; }
-    [Required]
     public string LastName  { get; set; }
     public string Description  { get; set; }
-    [Required]
     public string Study { get; set; }
-    [Required]
     public string ResumeUrl { get; set; }
     public string ProfilePhotoUrl { get; set; }
 
