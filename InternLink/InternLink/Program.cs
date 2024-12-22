e6e89fab8fc16df5fbc0ab574dce30576765774c
matecarl <113537267+matecarl@users.noreply.github.com>
2024-12-22 17:34:05 +0100

    Create delete functionality & confirmation modal
    Signed-off-by: matecarl <113537267+matecarl@users.noreply.github.com>

--- InternLink/InternLink/Program.cs
+++ InternLink/InternLink/Program.cs
@@ -20,8 +20,6 @@ builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
-builder.Services.AddQuickGridEntityFrameworkAdapter();
-
 builder.Services.AddAuthentication(options =>
     {
         options.DefaultScheme = IdentityConstants.ApplicationScheme;
