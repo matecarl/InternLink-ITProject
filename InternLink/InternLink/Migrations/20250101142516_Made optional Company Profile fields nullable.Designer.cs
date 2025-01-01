8b5e4143ecf947950c2e83e868ea3ba00ad2c8a0
matecarl <113537267+matecarl@users.noreply.github.com>
2025-01-01 15:28:58 +0100

    Add remaining migration for optional company fields
    Signed-off-by: matecarl <113537267+matecarl@users.noreply.github.com>

--- InternLink/InternLink/Migrations/20250101142516_Made optional Company Profile fields nullable.Designer.cs
+++ InternLink/InternLink/Migrations/20250101142516_Made optional Company Profile fields nullable.Designer.cs
@@ -4,6 +4,7 @@ using InternLink.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InternLink.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250101142516_Made optional Company Profile fields nullable")]
+    partial class MadeoptionalCompanyProfilefieldsnullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
