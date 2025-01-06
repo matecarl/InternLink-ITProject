cb985baecf33155bcb60f02c5780396c6ffd28b6
matecarl <113537267+matecarl@users.noreply.github.com>
2025-01-06 18:38:04 +0100

    Create new Model migrations
    Signed-off-by: matecarl <113537267+matecarl@users.noreply.github.com>

---
+++ InternLink/InternLink/Migrations/20250105214658_Created additional relationships for easily accessing swipes & profiles from linked entity.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternLink.Migrations
+{
+    /// <inheritdoc />
+    public partial class Createdadditionalrelationshipsforeasilyaccessingswipesprofilesfromlinkedentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Swipes_Internships_InternshipId",
+                table: "Swipes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StudentProfileId",
+                table: "Swipes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Swipes_StudentProfileId",
+                table: "Swipes",
+                column: "StudentProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Swipes_Internships_InternshipId",
+                table: "Swipes",
+                column: "InternshipId",
+                principalTable: "Internships",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Swipes_StudentProfiles_StudentProfileId",
+                table: "Swipes",
+                column: "StudentProfileId",
+                principalTable: "StudentProfiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Swipes_Internships_InternshipId",
+                table: "Swipes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Swipes_StudentProfiles_StudentProfileId",
+                table: "Swipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Swipes_StudentProfileId",
+                table: "Swipes");
+
+            migrationBuilder.DropColumn(
+                name: "StudentProfileId",
+                table: "Swipes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Swipes_Internships_InternshipId",
+                table: "Swipes",
+                column: "InternshipId",
+                principalTable: "Internships",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ InternLink/InternLink/Migrations/20250106154033_Matches & Swipe inactivity field.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternLink.Migrations
+{
+    /// <inheritdoc />
+    public partial class MatchesSwipeinactivityfield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Accepted",
+                table: "Swipes",
+                newName: "IsInactive");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAccepted",
+                table: "Swipes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Matches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InternshipId = table.Column<int>(type: "int", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    StudentProfileId = table.Column<int>(type: "int", nullable: false),
+                    MatchedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Matches_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Matches_Internships_InternshipId",
+                        column: x => x.InternshipId,
+                        principalTable: "Internships",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Matches_StudentProfiles_StudentProfileId",
+                        column: x => x.StudentProfileId,
+                        principalTable: "StudentProfiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_InternshipId",
+                table: "Matches",
+                column: "InternshipId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_StudentProfileId",
+                table: "Matches",
+                column: "StudentProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_UserId",
+                table: "Matches",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Matches");
+
+            migrationBuilder.DropColumn(
+                name: "IsAccepted",
+                table: "Swipes");
+
+            migrationBuilder.RenameColumn(
+                name: "IsInactive",
+                table: "Swipes",
+                newName: "Accepted");
+        }
+    }
+}
